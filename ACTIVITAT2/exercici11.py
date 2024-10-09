b38d2b5f91f9dce1f521049b721050e20074dc93
Albert <albertpencas@gmail.com>
2024-10-09 20:29:18 +0200

    Activitat 1 finalitzada. Fet per Albert PC

---
+++ ACTIVITAT2/exercici11.py
@@ -0,0 +1,4 @@
+num= int(input("Escriu un número: "))
+for n in range(0,11):
+    res=num*n
+    print(res)
---
+++ ACTIVITAT2/exercici6.py
@@ -0,0 +1,8 @@
+cont=1
+while cont<=100:
+    print (cont)
+    cont+=1
+
+
+for num in range(1,101):
+    print (num)
