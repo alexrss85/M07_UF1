482db3fe8cdeb7b9e79fce52cabb9b3b4d841f37
alexrss85 <2023_alex.rosendo@iticbcn.cat>
2024-10-14 20:50:40 +0200

    main Alex

--- ACTIVITAT4/main.py
+++ ACTIVITAT4/main.py
@@ -1,22 +1,12 @@
 from exercici3 import matriuNumsAleatoris,maximMatriu,minimMatriu
 from exercici4_Alex_Rosendo import ex4Alex
 
-from exercici1 import ex1_Albert
-from exercici4_Albert_Penades import ex4Albert
-
-from exercici2 import ex1,ex2,ex3
-from exercici4_Victor_Fernandez import ex4Victor
-
 import numpy as np
 
 #EX1(Albert)
-ex1_Albert()
+
 
 #EX2(Victor)
-
-ex1()
-ex2()
-ex3()
 
 #EX3 (Alex)
 matriu=matriuNumsAleatoris()
@@ -28,7 +18,7 @@ minimMatriu(matriu)
 ex4Alex()
 
 # #EX4 (Albert)
-# ex4Albert()
+
 
 # #EX4 (Victor)
-# ex4Victor()
+
