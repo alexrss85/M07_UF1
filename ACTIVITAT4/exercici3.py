920d2d95aac443b63cc97946761a60b5064fc241
alexrss85 <2023_alex.rosendo@iticbcn.cat>
2024-10-14 20:23:31 +0200

    rama alex

--- ACTIVITAT4/exercici3.py
+++ ACTIVITAT4/exercici3.py
@@ -1,1 +1,18 @@
-xd
+import numpy as np
+# Matriu de nums aleatoris de l'1 al 100
+def matriuNumsAleatoris():
+    fil = int(input("Escriu el num de files: "))
+    col = int(input("Escriu el num de files: "))
+    matriu = np.random.randint(0,101, size=(fil, col))
+    return(matriu)
+
+
+# Num més alt d'una matriu
+def maximMatriu(matriu):
+    print("El màxim es : "+str(np.max(matriu)))
+
+# Mínim d'una matriu
+def minimMatriu(matriu):
+    print("El mínim es : "+str(np.min(matriu)))
+
+
